71464514037cbf038a9c48601297e0018e5d853a
Austin F <93173601+msudawg07@users.noreply.github.com>
2025-04-02 14:14:51 -0500

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,84 @@
+let value;
+let value1;
+let value2;
+let opStore;
+let screen = document.querySelector('#screen')
+let numbers = document.querySelectorAll('.numbers');
+let operators = document.querySelectorAll('.operator');
+
+
+for(let number of numbers){
+number.addEventListener('click', () => {
+ value = screen.innerText += number.innerText;
+})
+}
+
+
+for(let operator of operators){
+   operator.addEventListener('click', () => {
+     value1 = value;
+     if(value1 && value2){
+       console.log("value 1: " + value1)
+        screen.innerText = operate(opStore, value1, value2);
+        value1 = screen.innerText;
+        opStore = operator.innerText;
+        console.log(opStore);
+        screen.innerText = '';
+        value2 = value1;
+      }
+      else if(value1){
+        opStore = operator.innerText;
+        console.log(opStore)
+        screen.innerText = '';
+        value2 = value1;
+      }
+   })
+ }
+
+ document.querySelector('.equals').addEventListener('click', () => {
+   if(value1 && value2){
+     screen.innerText = operate(opStore, value, value2);
+   }
+})
+
+
+document.querySelector('.clear').addEventListener('click', () => {
+  screen.innerText = '';
+  value = '';
+  value1 = '';
+  value2 = '';
+  opStore = '';
+})
+
+document.querySelector('.delete').addEventListener('click', () => {
+  screen.innerText = screen.innerText.slice(0, -1);
+})
+
+
+function operate(operator, x, y){
+  if(operator == '+'){
+    return add(x, y)
+  }else if(operator == '-'){
+    return subtract(y, x)
+  }else if(operator == 'x'){
+    return multiply(x, y)
+  }else if(operator == '÷'){
+    return divide(x, y)
+  }
+}
+
+function add(x, y){
+  return Number(x) + Number(y);
+}
+
+function subtract(x, y){
+  return x - y;
+}
+
+function multiply(x, y){
+  return x * y;
+}
+
+function divide(x, y){
+  return x / y;
+}
